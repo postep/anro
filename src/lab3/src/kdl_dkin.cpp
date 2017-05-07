b2c2500746beef18712c904a7aace15955f587bf
JPosteps <radek.gryta@gmail.com>
2017-05-08 01:59:11 +0200

    kdl

--- src/lab3/src/kdl_dkin.cpp
+++ src/lab3/src/kdl_dkin.cpp
@@ -10,47 +10,44 @@ using namespace std;
 using namespace KDL;
 
 void callback(const sensor_msgs::JointState & msg);
-void CalculateQuaternion();
 
 double t1,t2,t3;
 double a2=0,a3=0,d1=0;
-double Quat[4];
-double M[3][3];
+double Quaternion[4];
+double Matrix[3][3];
 double pos[3];
 
-void calculateQuaternion(double T[][3]);
+void calcQuat(double T[][3]);
 
 int main(int argc, char **argv)
-{	
-    //inicjacja ros
-	ros::init(argc, argv, "kdl");
-	
+{
+	ros::init(argc, argv, "KDL_DKIN");	
 	ros::NodeHandle n;
 	
-    //utworzenie pub i sub
 	ros::Publisher kdl_pub=n.advertise<geometry_msgs::PoseStamped>("posestamped_kdl",1);
-	ros::Subscriber kdl_sub=n.subscribe("joint_states",1000,callback);	
+	ros::Subscriber kdl_sub=n.subscribe("/joint_states",100,callback);
+
+	n.param<double>("a2",a2,0.4);
+	n.param<double>("a3",a3,0.2);
+	n.param<double>("d1",d1,0.2);
     
-    //pobranie parametrów
-    n.param<double>("a2",a2,0.4);
-    n.param<double>("a3",a3,0.2);
-    n.param<double>("d1",d1,0.2);
-    
-	ros::Rate loop_rate(100);
+	ros::Rate loop_rate(30);
 	geometry_msgs::PoseStamped msg;
 	
-	//stworzenie łancucha konematycznego
+//łańcuch KDL
 	KDL::Chain chain;
-	t1=0;	t2=M_PI/4.0; t3=M_PI/4.0;	
+	t1=0;
+	t2=M_PI/4.0;
+	t3=M_PI/4.0;	
 	
-	chain.addSegment(Segment(Joint(Joint::None),Frame(Frame::DH(0,0,0,0)))); //a, alpha, d, theta
+	chain.addSegment(Segment(Joint(Joint::None),Frame(Frame::DH(0,0,0,0))));
 	chain.addSegment(Segment(Joint(Joint::RotZ),Frame(Frame::DH(0,0,1.0,0))));
 	chain.addSegment(Segment(Joint(Joint::None),Frame(Frame::DH(0,-M_PI/2.0,0,0))));
 	chain.addSegment(Segment(Joint(Joint::RotZ),Frame(Frame::DH(1,0,0,-M_PI/2.0))));
 	chain.addSegment(Segment(Joint(Joint::RotZ),Frame(Frame::DH(0,0,0,0))));	
 	
 	
-	//utworzenie solveraros 
+//solver
 	ChainFkSolverPos_recursive solver(chain);
 	JntArray q(chain.getNrOfJoints());
 	Frame F_results;
@@ -58,42 +55,39 @@ int main(int argc, char **argv)
 		
 	while(ros::ok())
 	{
-	    ROS_DEBUG_STREAM("i "<<chain.getNrOfJoints());
-	    
-	    q(0)=t1;
-	    q(1)=t2;
-      	    q(2)=t3;
+		q(0)=t1;
+		q(1)=t2;
+		q(2)=t3;
        	
-      	solver.JntToCart(q,F_results);
-       	int k=0;
-       	for(int j=0;j<3;j++) {
+	      	solver.JntToCart(q,F_results);
+	       	int k=0;
+		for(int j=0;j<3;j++) {
 			pos[j]=F_results.p.data[j];
 			
 				for(int i=0;i<3;i++) {
-		        	M[j][i]=F_results.M.data[k];
+		        	Matrix[j][i]=F_results.M.data[k];
 			        k++;
 		        }   
 		}
        	
-       	calculateQuaternion(M);
-		//wypełenienie wiadomości treścią
+       		calcQuat(Matrix);
+//wiadomość do publishera
 		msg.header.frame_id="base_link";
-		msg.pose.orientation.x=Quat[0];
-		msg.pose.orientation.y=Quat[1];
-		msg.pose.orientation.z=Quat[2];
-		msg.pose.orientation.w=Quat[3];
+
 		msg.pose.position.x=pos[0];
 		msg.pose.position.y=pos[1];
 		msg.pose.position.z=pos[2];
-	    
-	    //publish
+
+		msg.pose.orientation.x=Quaternion[0];
+		msg.pose.orientation.y=Quaternion[1];
+		msg.pose.orientation.z=Quaternion[2];
+		msg.pose.orientation.w=Quaternion[3];
+
 		kdl_pub.publish(msg);
-	
+
 		ros::spinOnce();
-	
 		loop_rate.sleep();
 	}
-
 	return 0;
 }
 
@@ -105,35 +99,34 @@ void callback(const sensor_msgs::JointState & msg)
 			
 } 
 
-void calculateQuaternion(double T[][3])
+void calcQuat(double T[][3])
 {
-  float trace = T[0][0] + T[1][1] + T[2][2]; 
-  if( trace > 0 ) {
-    float s = 0.5f / sqrtf(trace+ 1.0f);
-    Quat[3] = 0.25f / s;
-    Quat[0] = ( T[2][1] - T[1][2] ) * s;
-    Quat[1] = ( T[0][2] - T[2][0] ) * s;
-    Quat[2] = ( T[1][0] - T[0][1] ) * s;
-  } else {
-    if ( T[0][0] > T[1][1] && T[0][0] > T[2][2] ) {
-      float s = 2.0f * sqrtf( 1.0f + T[0][0] - T[1][1] - T[2][2]);
-      Quat[3] = (T[2][1] - T[1][2] ) / s;
-      Quat[0] = 0.25f * s;
-      Quat[1] = (T[0][1] + T[1][0] ) / s;
-      Quat[2] = (T[0][2] + T[2][0] ) / s;
-    } else if (T[1][1] > T[2][2]) {
-      float s = 2.0f * sqrtf( 1.0f + T[1][1] - T[0][0] - T[2][2]);
-      Quat[3] = (T[0][2] - T[2][0] ) / s;
-      Quat[0] = (T[0][1] + T[1][0] ) / s;
-      Quat[1] = 0.25f * s;
-      Quat[2] = (T[1][2] + T[2][1] ) / s;
-    } else {
-      float s = 2.0f * sqrtf( 1.0f + T[2][2] - T[0][0] - T[1][1] );
-      Quat[3] = (T[1][0] - T[0][1] ) / s;
-      Quat[0] = (T[0][2] + T[2][0] ) / s;
-      Quat[1] = (T[1][2] + T[2][1] ) / s;
-      Quat[2] = 0.25f * s;
-    }
-  }
-  
+	float trace = T[0][0] + T[1][1] + T[2][2]; 
+	if( trace > 0 ) {
+		float s = 0.5f / sqrtf(trace+ 1.0f);
+		Quaternion[3] = 0.25f / s;
+		Quaternion[0] = ( T[2][1] - T[1][2] ) * s;
+		Quaternion[1] = ( T[0][2] - T[2][0] ) * s;
+		Quaternion[2] = ( T[1][0] - T[0][1] ) * s;
+	} else {
+		if ( T[0][0] > T[1][1] && T[0][0] > T[2][2] ) {
+			float s = 2.0f * sqrtf( 1.0f + T[0][0] - T[1][1] - T[2][2]);
+			Quaternion[3] = (T[2][1] - T[1][2] ) / s;
+			Quaternion[0] = 0.25f * s;
+			Quaternion[1] = (T[0][1] + T[1][0] ) / s;
+			Quaternion[2] = (T[0][2] + T[2][0] ) / s;
+		} else if (T[1][1] > T[2][2]) {
+			float s = 2.0f * sqrtf( 1.0f + T[1][1] - T[0][0] - T[2][2]);
+			Quaternion[3] = (T[0][2] - T[2][0] ) / s;
+			Quaternion[0] = (T[0][1] + T[1][0] ) / s;
+			Quaternion[1] = 0.25f * s;
+			Quaternion[2] = (T[1][2] + T[2][1] ) / s;
+		} else {
+			float s = 2.0f * sqrtf( 1.0f + T[2][2] - T[0][0] - T[1][1] );
+			Quaternion[3] = (T[1][0] - T[0][1] ) / s;
+			Quaternion[0] = (T[0][2] + T[2][0] ) / s;
+			Quaternion[1] = (T[1][2] + T[2][1] ) / s;
+			Quaternion[2] = 0.25f * s;
+		}
+	}
 }
