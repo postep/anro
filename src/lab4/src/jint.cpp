eb6d96fbba509fb20957a357bd7dac273023c426
osboxes.org <jakub.postepski@gmail.com>
2017-05-16 06:33:27 +0100

    path generation

--- src/lab4/src/jint.cpp
+++ src/lab4/src/jint.cpp
@@ -3,17 +3,24 @@
 #include <nav_msgs/Path.h>
 #include "lab4/jint_control_srv.h"
 #include <math.h>
+#include <kdl/chain.hpp>
+#include <kdl/chainfksolverpos_recursive.hpp>
+#include <kdl/frames.hpp>
 
 
 #define LOOP_RATE 30
+
+using namespace std;
+using namespace KDL;
 
 enum ITYPE {linear, spline};
 double old_joints[3];
 uint seq_joint_no = 0;
 uint seq_path_no = 0;
+uint seq_pose_no = 0;
 ros::Publisher joint_states_pub;
 ros::Publisher path_pub;
-nav_msgs::Path path;
+KDL::Chain chain;
 
 double calculate_interpolation(double x1, double x2, double t, double T, ITYPE interpolation){
   double ret;
@@ -25,25 +32,43 @@ double calculate_interpolation(double x1, double x2, double t, double T, ITYPE i
 }
 
 
-void fill_header(std_msgs::Header & header, int seq_no){
+void fill_header(std_msgs::Header & header, uint& seq_no){
   header.seq = seq_no;
-  header.frame_id="";
+  header.frame_id="base_link";
   header.stamp = ros::Time::now();
+  ++seq_no;
 }
 
 void fill_joint_message(sensor_msgs::JointState &msg, double j1, double j2, double j3){
-  fill_header(msg.header, seq_joint_no);
   msg.name.push_back("base_link_cylinder_1");
   msg.name.push_back("cylinder_1_cylinder_2");
   msg.name.push_back("cylinder_2_cylinder_3");
   msg.position.push_back(j1);
   msg.position.push_back(j2);
   msg.position.push_back(j3);
-  seq_joint_no++;
 }
 
 void add_path_position(nav_msgs::Path &path, double j1, double j2, double j3){
-  path.poses.push_back()
+  ChainFkSolverPos_recursive solver(chain);
+  JntArray q(3);
+  Frame F_results;
+  q(0)=j1;
+  q(1)=j2;
+  q(2)=j3;   	
+  solver.JntToCart(q, F_results);
+
+  geometry_msgs::PoseStamped pose_stamped;
+  fill_header(pose_stamped.header, seq_pose_no);
+  pose_stamped.pose.position.x = F_results.p.data[0];
+  pose_stamped.pose.position.y = F_results.p.data[1];
+  pose_stamped.pose.position.z = F_results.p.data[2];
+  double x, y, z, w;
+  F_results.M.GetQuaternion(x, y, z, w);
+  pose_stamped.pose.orientation.x=x;
+  pose_stamped.pose.orientation.y=y;
+  pose_stamped.pose.orientation.z=z;
+  pose_stamped.pose.orientation.w=w;
+  path.poses.push_back(pose_stamped);
 }
 
 bool interpolate(lab4::jint_control_srv::Request& request, lab4::jint_control_srv::Response& response)
@@ -83,6 +108,7 @@ bool interpolate(lab4::jint_control_srv::Request& request, lab4::jint_control_sr
   time_end = request.t;
   time_counter = 0;
   
+  nav_msgs::Path path;
   ros::Rate loop_rate(30);
   while(time_counter < time_end)
   {
@@ -92,8 +118,12 @@ bool interpolate(lab4::jint_control_srv::Request& request, lab4::jint_control_sr
     j3 = calculate_interpolation(old_joints[2], joints[2], time_counter, time_end, interpolation);
     
     sensor_msgs::JointState msg;
+    fill_header(msg.header, seq_joint_no);
     fill_joint_message(msg, j1, j2, j3);
+    fill_header(path.header, seq_path_no);
+    add_path_position(path, j1, j2, j3);
     joint_states_pub.publish(msg);
+    path_pub.publish(path);
     time_counter += 1/30.0;
     loop_rate.sleep();
   }
@@ -119,7 +149,12 @@ int main(int argc, char **argv)
   joint_states_pub.publish(msg);
 
   ros::ServiceServer service = n.advertiseService("oint_control_srv", interpolate);
-  
+
+	chain.addSegment(Segment(Joint(Joint::RotZ),Frame(Frame::DH(0,0,1.0,0))));
+	chain.addSegment(Segment(Joint(Joint::None),Frame(Frame::DH(0,-M_PI/2.0,0,0))));
+	chain.addSegment(Segment(Joint(Joint::RotZ),Frame(Frame::DH(1,0,0,-M_PI/2.0))));
+	chain.addSegment(Segment(Joint(Joint::RotZ),Frame(Frame::DH(0,0,0,0))));		
+
   ROS_INFO("Ready to interpolate.");
   ros::spin();
   return 0;
