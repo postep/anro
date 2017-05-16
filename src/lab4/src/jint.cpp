fe2d757010a9345e503f75aa9f00d815bef1c8a2
Jakub Postepski <jakub.postepski@gmail.com>
2017-05-16 05:48:28 +0100

    not working

--- src/lab4/src/jint.cpp
+++ src/lab4/src/jint.cpp
@@ -1,5 +1,6 @@
 #include <ros/ros.h>
 #include <sensor_msgs/JointState.h>
+#include <nav_msgs/Path.h>
 #include "lab4/jint_control_srv.h"
 #include <math.h>
 
@@ -8,8 +9,11 @@
 
 enum ITYPE {linear, spline};
 double old_joints[3];
-uint seq_no = 0;
+uint seq_joint_no = 0;
+uint seq_path_no = 0;
 ros::Publisher joint_states_pub;
+ros::Publisher path_pub;
+nav_msgs::Path path;
 
 double calculate_interpolation(double x1, double x2, double t, double T, ITYPE interpolation){
   double ret;
@@ -20,17 +24,26 @@ double calculate_interpolation(double x1, double x2, double t, double T, ITYPE i
   return ret;
 }
 
+
+void fill_header(std_msgs::Header & header, int seq_no){
+  header.seq = seq_no;
+  header.frame_id="";
+  header.stamp = ros::Time::now();
+}
+
 void fill_joint_message(sensor_msgs::JointState &msg, double j1, double j2, double j3){
-  msg.header.seq = seq_no;
-  msg.header.frame_id="";
-  msg.header.stamp = ros::Time::now();
+  fill_header(msg.header, seq_joint_no);
   msg.name.push_back("base_link_cylinder_1");
   msg.name.push_back("cylinder_1_cylinder_2");
   msg.name.push_back("cylinder_2_cylinder_3");
   msg.position.push_back(j1);
   msg.position.push_back(j2);
   msg.position.push_back(j3);
-  seq_no++;
+  seq_joint_no++;
+}
+
+void add_path_position(nav_msgs::Path &path, double j1, double j2, double j3){
+  path.poses.push_back()
 }
 
 bool interpolate(lab4::jint_control_srv::Request& request, lab4::jint_control_srv::Response& response)
@@ -40,10 +53,8 @@ bool interpolate(lab4::jint_control_srv::Request& request, lab4::jint_control_sr
   double time_end = 0;
   
   ROS_INFO("request: t1=%f, t2=%f, t3=%f", (float)request.t1, (float)request.t2, (float)request.t3);
-  if(time_counter < time_end){
-    //response.result = false;
-    return true;
-  }
+  ROS_INFO("interpolation type: %s", request.type.c_str());
+    
   if(request.t1 < -M_PI || request.t1 > M_PI || request.t2 < -M_PI || request.t2 > M_PI || request.t3 < -M_PI || request.t3 > M_PI){
     response.status = "Wrong joints";
     return true;
@@ -53,7 +64,6 @@ bool interpolate(lab4::jint_control_srv::Request& request, lab4::jint_control_sr
     return true;
   }
 
-  ROS_INFO("interpolation type: %s", request.type.c_str());
   ITYPE interpolation;
   if(request.type == "linear"){
     interpolation = linear;
@@ -103,6 +113,7 @@ int main(int argc, char **argv)
   ros::NodeHandle n;
   
   joint_states_pub=n.advertise<sensor_msgs::JointState>("joint_states",1);
+  path_pub=n.advertise<nav_msgs::Path>("path",1);
   sensor_msgs::JointState msg;
   fill_joint_message(msg, 0, 0, 0);
   joint_states_pub.publish(msg);
